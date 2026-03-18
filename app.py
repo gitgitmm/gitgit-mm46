7a603ec3561949937f21a4f807165edbc161cee8
Marija <marija.miljkoviic@gmail.com>
2026-03-18 10:49:15 +0100

    dodala sam NESTO

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
 import os
 import sqlite3
+from flask import Flask, request, jsonify, json
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Zdravo brate"
+@app.route("/primer-string")
+def string():
+    return "Neki ne preterano dugacak tekst"
+@app.route("/primer-broj")
+def broj():
+    return 265
+@app.route("/primer-niz")
+def niz():
+    nekiNiz = [1,2,3,4,5]
+    return nekiNiz
+@app.route("/primer-json")
+def primerJson():
+    data = {
+        "message": "This is a JSON response", 
+        "status": "successs"
+    }
