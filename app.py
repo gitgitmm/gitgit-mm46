16cb9b676fbb148b470248f6c2f95f40a1128a5d
gitgitmm <marija.miljkoviic@gmail.com>
2026-04-08 16:44:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import os
 import sqlite3
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Zdravo brate"
+    return render_template(index.html)
 @app.route("/primer-string")
 def string():
     return "Neki ne preterano dugacak tekst"
 @app.route("/primer-broj")
 def broj():
     return 265
-@app.route("/primer-niz")
-def niz():
-    nekiNiz = [1,2,3,4,5]
-    return nekiNiz
-@app.route("/primer-json")
-def primerJson():
-    data = {
-        "message": "This is a JSON response", 
-        "status": "successs"
-    }
+if __name__=="__main__":
+    app.run(debug=True)
